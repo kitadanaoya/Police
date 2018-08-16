0a11d02ee21f55571f8db3ed32ba282f00c0b6ab
kitadanaoya <naoya0206k@yahoo.co.jp>
2018-08-16 16:18:53 +0000

    feature: users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root  'lps#home'
+  get '/signup', to: 'users#new'
   get 'lps/home'
-
+  
+  root to:  'lps#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to: "application#hello"
 end
