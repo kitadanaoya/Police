b21cb10c69da206cd16e35c150669b173b2b9db6
kitadanaoya <naoya0206k@yahoo.co.jp>
2018-08-30 18:53:30 +0000

    feature:signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  post '/signup', to: 'users#create'
   get '/signup', to: 'users#new'
   get 'lps/home'
-  
   root to:  'lps#home'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to: "application#hello"
 end
